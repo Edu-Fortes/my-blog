239fddf43d6e56217c6f0040d75bd7a09324ae2d
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-12-17 10:17:04 -0300

    feat(header): add header section

---
+++ app/components/Header.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "../../public/next.svg";
+import style from "../components/style.module.css";
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <Image src={logo} alt="logo do blog" className={style.header__logo} />
+      <button type="button">Toggle</button>
+    </header>
+  );
+}
