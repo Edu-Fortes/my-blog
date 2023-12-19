b007f798c6ec971b897e3e93447d224c0a7b8aba
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-12-19 20:36:38 -0300

    feat(navbar): create navbar component

---
+++ app/components/Navbar.js
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li>Logo</li>
+        <li>Home</li>
+        <li>Artigos</li>
+        <li>Barra de Pesquisa</li>
+      </ul>
+    </nav>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import Main from "./components/Main";
 import Header from "./components/Header";
 import PostCard from "./components/PostCard";
 import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Header />
       <Main>
         <PostCard />
